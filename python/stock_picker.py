70fcf2c54e6dfd55de02038415fe55ce206c9bc8
kayleeburch <kayleemburch@gmail.com>
2022-06-06 17:35:45 -0400

    python solution added

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,11 @@
 def picker(prices):
-    pass
+    biggest_profit = 0
+    result = []
+    for i in range(len(prices)):
+        buy_date = i
+        for j in range(i + 1, len(prices)):
+            sell_date = j
+            if prices[j] - prices[i] > biggest_profit:
+                biggest_profit = prices[j] - prices[i]
+                result = [buy_date, sell_date]
+    return result
